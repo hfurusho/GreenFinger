0003c314c0adb7775f8cdd0a614a4753c1120379
Jiahui Wang <wjh4289042@hotmail.com>
2020-02-28 15:00:01 -0800

    table page

---
+++ client/src/pages/Table.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+// Generate Order Data
+function createData(id, img, name, location) {
+  return {id, img, name, location};
+}
+
+const rows = [
+  createData(0, 'cactus.jpg', 'cactus', 'kitchen'),
+  createData(1, 'zz.jpg', 'zz plant', 'livingroom'),
+  createData(2, 'snakePlant.jpg', 'snake plant', 'bathroom'),
+  createData(3, 'monstera.jpg', 'monstera', 'livingroom'),
+  createData(4, 'pothos.jpg', 'pothos', 'bedroom'),
+];
+
+function preventDefault(event) {
+  event.preventDefault();
+}
+
+const useStyles = makeStyles(theme => ({
+  seeMore: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+export default function FullTable() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <h1>My Plants：</h1>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Image</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Location</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.id}>
+              <TableCell>{row.img}</TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.location}</TableCell>
+              {/* <TableCell align="right">{row.amount}</TableCell> */}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      {/* <div className={classes.seeMore}>
+        <Link color="primary" href="#" onClick={preventDefault}>
+          See more orders
+        </Link>
+      </div> */}
+    </React.Fragment>
+  );
+}
