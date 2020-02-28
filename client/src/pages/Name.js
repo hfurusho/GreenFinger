4272c00043cf2b3046c91eb2995a13aee7fb3b7f
Jiahui Wang <wjh4289042@hotmail.com>
2020-02-28 10:31:51 -0800

    signup page

--- client/src/pages/Name.js
+++ client/src/pages/Name.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import TextField from "../components/TextField";
+import {Link,Typography} from '@material-ui/core';
 import Button from "../components/ContinueBtn";
 
 function Name(props) {
+    const classes = useStyles();
     return (
+        
         <div style={{ textAlign: "center" }}>
 
-            <h1>What kind of plant?</h1>
+            <h1>What's the name of your plant?</h1>
 
             <TextField />
             <br />
-            <Button />
-
+            <Typography className={classes.root}>
+                <Link href="Location" onClick={preventDefault}>
+                    <Button />
+                </Link>
+            </Typography>
         </div>
-
-
     )
 }
 
