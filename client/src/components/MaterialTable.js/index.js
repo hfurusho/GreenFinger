3076df986d276bdc3f864ef10f6261698f659930
Arnold Diep <tungd@uw.edu>
2020-02-27 19:45:03 -0800

    testing material table

---
+++ client/src/components/MaterialTable.js/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import MaterialTable from 'material-table';
+
+export default function MaterialTable() {
+  const [state, setState] = React.useState({
+    columns: [
+      { title: 'Plant Name', field: 'name' },
+      { title: 'Location', field: 'location' },
+    ],
+    data: [],
+  });
+
+  return (
+    <MaterialTable
+      title="My Plants"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              setState(prevState => {
+                const data = [...prevState.data];
+                data.push(newData);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              if (oldData) {
+                setState(prevState => {
+                  const data = [...prevState.data];
+                  data[data.indexOf(oldData)] = newData;
+                  return { ...prevState, data };
+                });
+              }
+            }, 600);
+          }),
+        onRowDelete: oldData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              setState(prevState => {
+                const data = [...prevState.data];
+                data.splice(data.indexOf(oldData), 1);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+      }}
+    />
+  );
+}
