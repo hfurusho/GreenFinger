bd19047586796e42a988729836dbc30f93e58b0f
Jiahui Wang <wjh4289042@hotmail.com>
2020-02-28 17:22:43 -0800

    clickable rows and localstorage

--- client/src/pages/Name.js
+++ client/src/pages/Name.js
@@ -1,16 +1,54 @@
-import React from "react";
-import TextField from "../components/TextField";
+import React, { Component } from "react";
+// import BasicTextField from "../components/TextField";
 import Button from "../components/ContinueBtn";
+import { TextField } from "@material-ui/core";
+// import { makeStyles } from "@material-ui/core/styles";
 
-export default function Name(props) {
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h1>What's the name of your plant?</h1>
+export default class Name extends Component {
+  constructor(props) {
+    super(props);
+    // this.classes = this.useStyles();
+    this.saveName = this.saveName.bind(this);
+    this.state = {
+      name: ""
+    };
+    this.updateInput = this.updateInput.bind(this);
+  }
 
-      <TextField />
-      <br />
+//   useStyles = makeStyles(theme => ({
+//     root: {
+//       "& > *": {
+//         margin: theme.spacing(1),
+//         width: 200
+//       }
+//     }
+//   }));
 
-      <Button href="Location" />
-    </div>
-  );
+  updateInput(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+  saveName() {
+    alert("saveName executed");
+    //grab text from text field and store in local story
+    var name = this.state.name;
+    // alert(name);
+    localStorage.setItem("plantName",name);
+  }
+
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h1>What's the name of your plant?</h1>
+
+        <form>
+          <TextField id="name-field" label="" onChange={this.updateInput} />
+        </form>
+        <br />
+
+        <Button href="Location" onClick={this.saveName} />
+      </div>
+    );
+  }
 }
--- client/src/pages/Table.js
+++ client/src/pages/Table.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Link from '@material-ui/core/Link';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+import {NavLink} from "react-router-dom";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles({
-    table: {
-      minWidth: 650,
-    },
-  });  
+  table: {
+    minWidth: 650
+  }
+});
 
 // Generate Order Data
 function createData(id, img, name, location) {
-  return {id, img, name, location};
+  return { id, img, name, location };
 }
 
 const rows = [
-  createData(0, 'cactus.jpg', 'cactus', 'kitchen'),
-  createData(1, 'zz.jpg', 'zz plant', 'livingroom'),
-  createData(2, 'snakePlant.jpg', 'snake plant', 'bathroom'),
-  createData(3, 'monstera.jpg', 'monstera', 'livingroom'),
-  createData(4, 'pothos.jpg', 'pothos', 'bedroom'),
+  createData(0, "cactus.jpg", "cactus", "kitchen"),
+  createData(1, "zz.jpg", "zz plant", "livingroom"),
+  createData(2, "snakePlant.jpg", "snake plant", "bathroom"),
+  createData(3, "monstera.jpg", "monstera", "livingroom"),
+  createData(4, "pothos.jpg", "pothos", "bedroom")
 ];
 
 function preventDefault(event) {
@@ -54,12 +55,18 @@ export default function FullTable() {
 
         <TableBody>
           {rows.map(row => (
-            <TableRow key={row.id}>
-              <TableCell>{row.img}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.location}</TableCell>
-              {/* <TableCell align="right">{row.amount}</TableCell> */}
-            </TableRow>
+            // <Link href= {"/plants/" + row.id} onClick={preventDefault}>
+              <NavLink to={"/plants/" + row.id} data-some-attribute="some-value">
+
+              <TableRow key={row.id}>
+                <TableCell>{row.img}</TableCell>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.location}</TableCell>
+                {/* <TableCell align="right">{row.amount}</TableCell> */}
+              </TableRow>
+
+              </NavLink>
+            // </Link>
           ))}
         </TableBody>
       </Table>
@@ -68,6 +75,6 @@ export default function FullTable() {
           See more plants
         </Link>
       </div>
-      </TableContainer>
+    </TableContainer>
   );
 }
