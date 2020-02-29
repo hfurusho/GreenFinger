0ca586b934f9cce0c60074186bc16422d2887fb1
Kami <51175640+hfurusho@users.noreply.github.com>
2020-02-29 13:25:29 -0800

    Added secretOrKey config for passport.

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: process.env.MONGODB_URI || "mongodb://localhost/GreenThumb"
+  mongoURI: process.env.MONGODB_URI || "mongodb://localhost/GreenThumb",
+  secretOrKey: "secret"
 };
