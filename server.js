a38338fcca8ac46d09aec365521f442245478d4d
Kami <51175640+hfurusho@users.noreply.github.com>
2020-02-29 13:05:31 -0800

    Connection to db and bodyparsing for inc req.

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-// const mongoose = require("mongoose");
-// const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// MAYBE OPTIONAL???
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -15,10 +24,12 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 // app.use(routes);
 
+const db = require("./config/keys").mongoURI;
 // Connect to the Mongo DB
-// mongoose.connect(
-//   process.env.MONGODB_URI || "mongodb://localhost/DB_NAME_GOES_HERE"
-// );
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 // Start the API server
 app.listen(PORT, function() {
