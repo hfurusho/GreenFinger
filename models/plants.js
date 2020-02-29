26e041ead2acbcfc2bf9a35e900cadcb315410aa
Kami <51175640+hfurusho@users.noreply.github.com>
2020-02-29 13:03:58 -0800

    No change

--- models/plants.js
+++ models/plants.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const plantSchema = new Schema({
-    name: { type: String, required: true },
-    water: { type: Number, required: true},
-    location: String,
-    notes: String
+  name: { type: String, required: true },
+  water: { type: Number, required: true },
+  location: String,
+  notes: String
 });
 
 const Plant = mongoose.model("Plant", plantSchema);
