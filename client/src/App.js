69b22a75308a59db0926e52ad418b9a10eb30808
Jiahui Wang <wjh4289042@hotmail.com>
2020-03-03 16:20:03 -0800

    set up plantdetail

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ function App() {
           <Route exact path="/waterschedule" component={WaterSchedule} />
           <Route exact path="/notes" component={Notes} />
           <Route exact path="/table" component={Table} />
-          <Route exact path="/plants/:id" component={PlantDetail} />
+          <Route exact path="/plants/:name" component={PlantDetail} />
           <Route exact path="/table" component={Table} />
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/signin" component={SignIn} />
---
+++ client/src/pages/PlantDetail.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+
+export default function PlantDetail() {
+    return(
+        <Container>
+            <h1>Plant's name</h1>
+            <h2>Location:</h2>
+            <h2>last watered on:</h2>
+            <h2>Next water date:</h2>
+            <h2>Addition notes: </h2>
+            <Link color="primary" href="WaterSchedule">
+                Change Water Schedule
+            </Link>
+
+
+
+            
+        </Container>
+
+    )
+}
