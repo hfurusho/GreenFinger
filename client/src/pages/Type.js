184b28543a74840809d6453b5b43290bf863c034
Jiahui Wang <wjh4289042@hotmail.com>
2020-03-05 00:29:35 -0800

    api call works on normans mine not working weird

--- client/src/pages/Type.js
+++ client/src/pages/Type.js
@@ -16,7 +16,6 @@ export default function Type() {
     console.log("searchImage fired");
     const queryUrl = `https://pixabay.com/api/?key=15485203-cba91f318bb796f35c4848942&q=${type}&image_type=photo&pretty=true`;
     Axios.get(queryUrl)
-      .then(res => res.json()) //do I need this?
       .then(function(res){
         const imageUrl = res.data.hits[0].webformatURL;
         console.log("api call result", res);
