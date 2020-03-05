5f407341f03c47dfb4bf9a07beee6a689c43688c
Kami <51175640+hfurusho@users.noreply.github.com>
2020-03-04 19:32:19 -0800

    Add GlobalState Provider

--- client/src/App.js
+++ client/src/App.js
@@ -14,36 +14,38 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import NoMatch from "./pages/NoMatch";
 
-import rootReducer from "./reducers";
 //import "./App.css";
+import GlobalState from "./GlobalState";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/name" component={Name} />
-          <Route exact path="/location" component={Location} />
-          <Route exact path="/waterschedule" component={WaterSchedule} />
-          <Route exact path="/notes" component={Notes} />
-          <Route exact path="/table" component={Table} />
-          <Route exact path="/plants/:name" component={PlantDetail} />
-          <Route exact path="/table" component={Table} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/signin" component={SignIn} />
-          <Route exact path="/user" component={User} />
+    <GlobalState>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/name" component={Name} />
+            <Route exact path="/location" component={Location} />
+            <Route exact path="/waterschedule" component={WaterSchedule} />
+            <Route exact path="/notes" component={Notes} />
+            <Route exact path="/table" component={Table} />
+            <Route exact path="/plants/:name" component={PlantDetail} />
+            <Route exact path="/table" component={Table} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/user" component={User} />
 
-          <Route component={NoMatch} />
-        </Switch>
+            <Route component={NoMatch} />
+          </Switch>
 
-        <Grid container justify="center">
-          <Grid item>
-            <Navbar />
+          <Grid container justify="center">
+            <Grid item>
+              <Navbar />
+            </Grid>
           </Grid>
-        </Grid>
-      </div>
-    </Router>
+        </div>
+      </Router>
+    </GlobalState>
   );
 }
 
