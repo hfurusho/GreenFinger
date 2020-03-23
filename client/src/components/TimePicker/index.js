fc7edb356b876cacd3aa23f7c3fc1b08863e3a8b
Kami <51175640+hfurusho@users.noreply.github.com>
2020-03-23 15:54:04 -0700

    Code reformat.

--- client/src/components/TimePicker/index.js
+++ client/src/components/TimePicker/index.js
@@ -1,22 +1,24 @@
-import 'date-fns';
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import DateFnsUtils from '@date-io/date-fns';
-import { MuiPickersUtilsProvider, KeyboardTimePicker} from '@material-ui/pickers';
+import "date-fns";
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker
+} from "@material-ui/pickers";
 
 export default function MaterialUIPickers(props) {
-
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <Grid container justify="space-around">
-      <KeyboardTimePicker
+        <KeyboardTimePicker
           margin="normal"
           id="time-picker"
           label=""
           value={props.selectedTime}
           onChange={props.onTimeChange}
           KeyboardButtonProps={{
-            'aria-label': 'change time',
+            "aria-label": "change time"
           }}
         />
       </Grid>
