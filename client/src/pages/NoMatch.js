eaec96098820084ee469ac26137f679bf1683395
Kami <51175640+hfurusho@users.noreply.github.com>
2020-03-25 14:25:57 -0700

    Code cleanup

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,17 +1,12 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 
-
-export default function NoMatch () {
+export default function NoMatch() {
   return (
     <Container fluid>
-      {/* <Row>
-        <Col size="md-12"> */}
-          <div>
-            <h1>404 Page Not Found</h1>
-          </div>
-        {/* </Col> */}
-      {/* </Row> */}
+      <div>
+        <h1>404 Page Not Found</h1>
+      </div>
     </Container>
   );
-};
+}
